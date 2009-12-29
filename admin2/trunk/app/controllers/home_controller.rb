60666ee3a623f705561466ec76a7f340a9c02e2f
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2009-12-29 14:52:59 +0000

    Update the home controller to add info inside session and translation
    git-svn-id: file:///svn/openvisp@241 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin2/trunk/app/controllers/home_controller.rb
+++ admin2/trunk/app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  def available_locales; AVAILABLE_LOCALES; end
+
+  before_filter :set_locale
+  def set_locale
+    # if params[:locale] is nil then I18n.default_locale will be used
+    I18n.locale = params[:locale]
+  end
+  
   def index
     if defined? session[:username].length
       redirect_to :action => "controlboard"
@@ -10,15 +18,25 @@ class HomeController < ApplicationController
     @accounts = Account.find(:all, :conditions => { :username => params[:account][:username], :password => params[:account][:password], :enabled => "1"})
     if @accounts.count == 1
       session[:username] = @accounts[0].username
-      flash[:notice] = "Successfully logged in"  
+      session[:lastname] = @accounts[0].lastname
+      session[:firstname] = @accounts[0].firstname
+      flash[:notice] = 'Successfully logged in'  
       redirect_to :action => "index"
     else
-      flash[:notice] = "Sorry you have enter invalid credential or your account was disabled"
+      flash[:notice] = 'Sorry you have enter invalid credential or your account was disabled'
       redirect_to :action => "index"
     end
   end
   
   def controlboard
-    
+    @welcome_info = "(#{session[:firstname]} #{session[:lastname]})"
+    @logout = t(:logout)
   end
+  
+  def logout
+    reset_session  
+    flash[:notice] = 'Successfully logged out'  
+    redirect_to :action => "index"
+  end
+  
 end
