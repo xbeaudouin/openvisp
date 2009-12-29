f7c8fbd05ebbce3ffba2f1c3cc27600b6c07376d
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2009-12-29 15:00:43 +0000

    Add locale initiator to use locale inside OVA
    git-svn-id: file:///svn/openvisp@243 bed1d25c-e804-df11-9dd7-000c2933e665

---
+++ admin2/trunk/config/initializers/locale.rb
@@ -0,0 +1,5 @@
+# tell the I18n library where to find your translations
+I18n.load_path << Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ]
+
+# set default locale to something other than :en
+#I18n.default_locale = :pt
