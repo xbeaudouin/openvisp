9cdd18d5fa06bbdd225aaafa3dd7c6be4abcd2e5
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-01-09 14:18:50 +0000

    Add the default values for account quota
    git-svn-id: file:///svn/openvisp@293 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin2/trunk/db/migrate/20100101150031_quota.rb
+++ admin2/trunk/db/migrate/20100101150031_quota.rb
@@ -2,15 +2,15 @@ class Quota < ActiveRecord::Migration
   def self.up
     change_table :quotas do |t|
       t.column :account_id,       :integer,    :null => false
-      t.column :diskspace,        :integer,    :null => false
-      t.column :ftp,              :integer,    :null => false
-      t.column :dbcount,          :integer,    :null => false
-      t.column :dbuser,           :integer,    :null => false
-      t.column :domains,          :integer,    :null => false
-      t.column :emails,           :integer,    :null => false
-      t.column :emails_alias,     :integer,    :null => false
-      t.column :http,             :integer,    :null => false
-      t.column :http_alias,       :integer,    :null => false 
+      t.column :diskspace,        :integer,    :null => false, :default => '0'
+      t.column :ftp,              :integer,    :null => false, :default => '0'
+      t.column :dbcount,          :integer,    :null => false, :default => '0'
+      t.column :dbuser,           :integer,    :null => false, :default => '0'
+      t.column :domains,          :integer,    :null => false, :default => '0'
+      t.column :emails,           :integer,    :null => false, :default => '0'
+      t.column :emails_alias,     :integer,    :null => false, :default => '0'
+      t.column :http,             :integer,    :null => false, :default => '0'
+      t.column :http_alias,       :integer,    :null => false, :default => '0'
     end
 
   end
