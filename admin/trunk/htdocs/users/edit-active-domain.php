d53e1dc8c34a720c4192240f555dfd37740f2d7f
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-04-16 13:42:47 +0000

    Change a lot of code to be OO.
    git-svn-id: file:///svn/openvisp@352 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/users/edit-active-domain.php
+++ admin/trunk/htdocs/users/edit-active-domain.php
@@ -17,12 +17,34 @@ require ("../config.inc.php");
 require ("../lib/functions.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+
 $SESSID_USERNAME = check_user_session();
+$ovadb = new DB();
+$userinfo = new USER($ovadb);
+$userinfo->fetch_info($SESSID_USERNAME);
+
+
+
 
 if ($_SERVER['REQUEST_METHOD'] == "GET")
 {
    $fDomain = get_get('domain');
-
+   $fAction = get_get('action');
+   
+   $domain_info = new DOMAIN($ovadb);
+   $domain_info->fetch_by_domainname($fDomain);
+   if ( $fAction == "activate" ) {
+     $domain_info->en_disable("+1");
+   }
+   else{
+     $domain_info->en_disable("-1");
+   }
+   
+/*
    $result = db_query ("UPDATE domain SET active=1-active WHERE domain='$fDomain'");
    if ($result['rows'] != 1)
    {
@@ -36,13 +58,15 @@ if ($_SERVER['REQUEST_METHOD'] == "GET")
    // When master domain is not active then set the mailboxes to off as well
    $result = db_query ("UPDATE mailbox, domain SET mailbox.active=domain.active  WHERE domain.domain='$fDomain' AND domain.id=mailbox.domain_id");
 
-   
+*/
+
    if ($error != 1)
    {
       header ("Location: list-domain.php");
       exit;
    }
-   
+
+
    include ("../templates/header.tpl");
    include ("../templates/users/menu.tpl");
    include ("../templates/message.tpl");
