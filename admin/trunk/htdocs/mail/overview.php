5bf30e42952f24902ebf7e4f94cdd80e331b0536
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-04-16 13:42:09 +0000

    Change a lot of code to be OO.
    git-svn-id: file:///svn/openvisp@351 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/mail/overview.php
+++ admin/trunk/htdocs/mail/overview.php
@@ -24,18 +24,41 @@ require ("../lib/functions.inc.php");
 require ("../lib/accounts.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
+
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+
 $SESSID_USERNAME = check_user_session ();
 
-$list_domains = list_domains_for_admin ($SESSID_USERNAME);
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$user_info->fetch_info($SESSID_USERNAME);
+$user_info->fetch_active_domains();
+$domain_info = new DOMAIN($ovadb);
 
-$account_information = get_account_info($SESSID_USERNAME);
-$account_quota = get_account_quota($account_information['id']);
-$account_rights = get_account_right($account_information['id']);
-$total_used = get_account_used($SESSID_USERNAME,check_admin($SESSID_USERNAME));
+$user_info->fetch_quota_status();
+
+$ajax_yui = new AJAX_YUI($ovadb);
+
+//$list_domains = list_domains_for_admin ($SESSID_USERNAME);
+
+//$account_information = get_account_info($SESSID_USERNAME);
+// replaced by fetch_info method
+
+//$account_quota = get_account_quota($account_information['id']);
+// replaced by fetch_quota method
+
+//$account_rights = get_account_right($account_information['id']);
+// replaced by fetch_rights
+
+//$total_used = get_account_used($SESSID_USERNAME,check_admin($SESSID_USERNAME));
 
 
-$tAlias = array();
-$tMailbox = array();
+//$tAlias = array();
+//$tMailbox = array();
 
 if ($_SERVER['REQUEST_METHOD'] == "GET")
 {
@@ -45,50 +68,30 @@ if ($_SERVER['REQUEST_METHOD'] == "GET")
    $fDisplay = get_get('limit');
    if($fDisplay == NULL) $fDisplay = 0;
 
-   if (($fDomain != NULL) && check_owner ($SESSID_USERNAME, $fDomain))
+   if ( $fDomain != NULL)
    {
-      $limit = get_domain_properties ($fDomain);
-   
-      if ($CONF['alias_control'] == "YES")
-      {
-         $sql_query = "SELECT alias.address,alias.goto,alias.modified,alias.policy_id FROM alias WHERE alias.domain='$fDomain' ORDER BY alias.address LIMIT $fDisplay, $page_size";
-      }
-      else
-      {
-				$sql_query = "SELECT alias.address, alias.goto, alias.modified, alias.policy_id
-FROM policy, domain, alias
-LEFT JOIN mailbox ON alias.address=mailbox.username
-WHERE domain.domain='$fDomain'
-AND domain.id=policy.domain_id
-AND policy.id=alias.policy_id
-AND mailbox.maildir IS NULL
-AND alias.address NOT LIKE '@%'
-ORDER BY alias.address
-LIMIT $fDisplay, $page_size";
-      }
+      //$limit = get_domain_properties ($fDomain);
 
+      $domain_info->fetch_by_domainname($fDomain);
+      $user_info->check_domain_access($domain_info->data_domain['id']);
+      $domain_info->fetch_mail_aliases();
+
+      /*
       $result = db_query ($sql_query);
+      
       if ($result['rows'] > 0)
       {
-         while ($row = db_array ($result['result']))
-         {
-            $tAlias[] = $row;
-         }
+      while ($row = db_array ($result['result']))
+      {
+      $tAlias[] = $row;
       }
+      }
+      */
 
 			//$result = db_query ("SELECT mailbox.*, alias.policy_id, spamreport.* FROM mailbox,alias,spamreport WHERE mailbox.domain='$fDomain' and mailbox.username=spamreport.email and mailbox.username=alias.address ORDER BY ".$CONF['order_display']." ASC LIMIT $fDisplay, $page_size");
 			//$tMailbox = list_mailbox($fDomain);
 
-			$sql_query = "SELECT mailbox.*, alias.policy_id, spamreport.*, vacation.active as vacation_active
-FROM alias, domain, mailbox
-LEFT OUTER JOIN spamreport ON ( mailbox.id = spamreport.mailbox_id )
-LEFT OUTER JOIN vacation ON ( mailbox.id = vacation.mailbox_id )
-WHERE domain.domain='$fDomain'
-AND domain.id=mailbox.domain_id
-AND mailbox.username=alias.address
-ORDER BY ".$CONF['order_display']." ASC
-LIMIT $fDisplay, $page_size";
-
+			/*
 			$result = db_query ($sql_query);
 
 
@@ -99,6 +102,11 @@ LIMIT $fDisplay, $page_size";
             $tMailbox[] = $row;
          }
       }
+      
+      */
+      
+      $domain_info->fetch_mailboxes();
+      
       $template = "overview.tpl";
    }
    else
@@ -107,24 +115,24 @@ LIMIT $fDisplay, $page_size";
    }
 
    $tDomain = $fDomain;
-
-   if (isset ($limit))
+   
+   /*
+   if ($fDisplay >= $page_size)
    {
-      if ($fDisplay >= $page_size)
-      {
-         $tDisplay_back_show = 1;
-         $tDisplay_back = $fDisplay - $page_size;
-      }
-      if (($limit['alias_count'] > $page_size) or ($limit['mailbox_count'] > $page_size))
-      {
-         $tDisplay_up_show = 1;
-      }      
-      if ((($fDisplay + $page_size) < $limit['alias_count']) or (($fDisplay + $page_size) < $limit['mailbox_count']))
-      {
-         $tDisplay_next_show = 1;
-         $tDisplay_next = $fDisplay + $page_size;
-      }
+     $tDisplay_back_show = 1;
+     $tDisplay_back = $fDisplay - $page_size;
+   }  
+   if (($domain_info->used_quota['mail_alias'] > $page_size) or ($domain_info->used_quota['mailbox'] > $page_size))
+   {
+     $tDisplay_up_show = 1;
+   }      
+   if ((($fDisplay + $page_size) < $domain_info->used_quota['mail_alias']) or (($fDisplay + $page_size) < $domain_info->used_quota['mailbox']))
+   {
+     $tDisplay_next_show = 1;
+     $tDisplay_next = $fDisplay + $page_size;
    }
+
+   */
    
    include ("../templates/header.tpl");
    include ("../templates/mail/menu.tpl");
@@ -141,95 +149,42 @@ if ($_SERVER['REQUEST_METHOD'] == "POST")
    if($fDisplay == NULL) $fDisplay = 0;
    $fDomain = get_post('fDomain');   
    
-   if (($fDomain != NULL) && check_owner ($SESSID_USERNAME, $fDomain))
+   if ($fDomain != NULL)
    {
-      $limit = get_domain_properties ($fDomain);
-      $domain_policy = get_domain_policy ($fDomain);
+      $domain_info->fetch_by_domainname($fDomain);
+      $user_info->check_domain_access($domain_info->data_domain['id']);
+      $domain_info->fetch_mail_aliases();
+      $domain_info->fetch_mailboxes();
+      
+      //$limit = get_domain_properties ($fDomain);
+      //$domain_policy = get_domain_policy ($fDomain);
 
       if ( $fMail_Search != NULL ) $pSearch_Data="AND alias.address LIKE '%$fMail_Search%'";
-   
-      if ($CONF['alias_control'] == "YES")
-      {
-         $query = "SELECT alias.address,alias.goto,alias.modified,alias.policy_id FROM alias WHERE alias.domain='$fDomain' $pSearch_Data ORDER BY alias.address LIMIT $fDisplay, $page_size";
-      }
-      else
-      {
-				$sql_query = "SELECT alias.address, alias.goto, alias.modified, alias.policy_id
-FROM policy, domain, alias
-LEFT JOIN mailbox ON alias.address=mailbox.username
-WHERE domain.domain='$fDomain'
-AND domain.id=policy.domain_id
-AND policy.id=alias.policy_id
-AND mailbox.maildir IS NULL
-$pSearch_Data
-ORDER BY alias.address
-LIMIT $fDisplay, $page_size";
-      }
+
+      //$domain_info->list_mail_aliases();
 
 #         $query = "SELECT alias.address,alias.goto,alias.modified,alias.policy_id FROM alias LEFT JOIN mailbox ON alias.address=mailbox.username WHERE alias.domain='$fDomain' AND mailbox.maildir IS NULL $pSearch_Data ORDER BY alias.address LIMIT $fDisplay, $page_size";
 
-      $result = db_query ("$sql_query");
-      if ($result['rows'] > 0)
-      {
-         while ($row = db_array ($result['result']))
-         {
-            $tAlias[] = $row;
-         }
-      }
-
-      if ( $fMail_Search != NULL ) $pSearch_Data="and mailbox.username like '%$fMail_Search%'";
-
-			//      $result = db_query ("SELECT mailbox.*, alias.policy_id, spamreport.* FROM mailbox,alias,spamreport WHERE mailbox.domain='$fDomain' and mailbox.username=spamreport.email and mailbox.username=alias.address ".$pSearch_Data." ORDER BY ".$CONF['order_display']." LIMIT $fDisplay, $page_size");
-
-			$sql_query = "SELECT mailbox.*, alias.policy_id, spamreport.*
-FROM alias, mailbox 
-LEFT OUTER JOIN spamreport ON ( mailbox.username = spamreport.email )
-WHERE mailbox.domain='$fDomain'
-  AND mailbox.username=alias.address
-  ".$pSearch_Data."
-ORDER BY ".$CONF['order_display']." ASC LIMIT $fDisplay, $page_size";
-
-			$sql_query = "SELECT mailbox.*, alias.policy_id, spamreport.*, vacation.active as vacation_active
-FROM alias, domain, mailbox
-LEFT OUTER JOIN spamreport ON ( mailbox.id = spamreport.mailbox_id )
-LEFT OUTER JOIN vacation ON ( mailbox.id = vacation.mailbox_id )
-WHERE domain.domain='$fDomain'
-AND domain.id=mailbox.domain_id
-AND mailbox.username=alias.address
-$pSearch_Data
-ORDER BY ".$CONF['order_display']." ASC
-LIMIT $fDisplay, $page_size";
-
-			$result = db_query ($sql_query);
-
-			//      $result = db_query ("SELECT mailbox.*, alias.policy_id FROM mailbox,alias WHERE mailbox.domain='$fDomain' and mailbox.username=alias.address ".$pSearch_Data." ORDER BY ".$CONF['order_display']." LIMIT $fDisplay, $page_size");
-
-      if ($result['rows'] > 0)
-      {
-         while ($row = db_array ($result['result']))
-         {
-            $tMailbox[] = $row;
-         }
-      }
    }
 
-   if (isset ($limit))
+   $tDomain = $fDomain;
+   
+   if ($fDisplay >= $page_size)
    {
-      if ($fDisplay >= $page_size)
-      {
-         $tDisplay_back_show = 1;
-         $tDisplay_back = $fDisplay - $page_size;
-      }
-      if (($limit['alias_count'] > $page_size) or ($limit['mailbox_count'] > $page_size))
-      {
-         $tDisplay_up_show = 1;
-      }
-      if ((($fDisplay + $page_size) < $limit['alias_count']) or (($fDisplay + $page_size) < $limit['mailbox_count']))
-      {
-         $tDisplay_next_show = 1;
-         $tDisplay_next = $fDisplay + $page_size;
-      }
+     $tDisplay_back_show = 1;
+     $tDisplay_back = $fDisplay - $page_size;
    }
+   if (($domain_info->used_quota['mail_alias'] > $page_size) or ($domain_info->used_quota['mailbox'] > $page_size))
+   {
+     $tDisplay_up_show = 1;
+   }      
+   if ((($fDisplay + $page_size) < $domain_info->used_quota['mail_alias']) or (($fDisplay + $page_size) < $domain_info->used_quota['mailbox']))
+   {
+     $tDisplay_next_show = 1;
+     $tDisplay_next = $fDisplay + $page_size;
+   }
+
+   
 
    include ("../templates/header.tpl");
    include ("../templates/mail/menu.tpl");
