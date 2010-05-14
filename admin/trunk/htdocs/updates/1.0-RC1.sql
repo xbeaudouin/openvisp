78ff0b3eb8d5b26550b61a4f614a75f5ab9a9e68
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-14 05:18:52 +0000

    Add some fix inside database.
    git-svn-id: file:///svn/openvisp@400 bed1d25c-e804-df11-9dd7-000c2933e665

---
+++ admin/trunk/htdocs/updates/1.0-RC1.sql
@@ -0,0 +1,18 @@
+
+ALTER TABLE alias ADD domain_id int(11);
+
+ALTER TABLE `alias` ADD INDEX (  `domain_id` )
+
+ALTER TABLE alias ADD FOREIGN KEY ( `domain_id` ) REFERENCES `domain` (`id`) ON DELETE CASCADE;
+
+CREATE VIEW TMP_ALIAS AS
+SELECT domain.id as domain_id, policy.id as policy_id, alias.address, alias.domain_id as alias_domain_id
+FROM alias, domain, policy
+WHERE domain.id = policy.domain_id
+AND policy.id = alias.policy_id;
+
+UPDATE TMP_ALIAS
+SET alias_domain_id = domain_id;
+
+DROP VIEW TMP_ALIAS;
+
