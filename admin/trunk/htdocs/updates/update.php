0d8b6d5497068edff6eaf3e17b5539ca12d42ef5
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-14 21:30:37 +0000

    Change for a new update system more easy.
    git-svn-id: file:///svn/openvisp@413 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/updates/update.php
+++ admin/trunk/htdocs/updates/update.php
@@ -8,136 +8,30 @@ require ("../lib/functions.inc.php");
 require ("../lib/versions.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
-$dir= "./";
- 
-$versionfrom = versionfrom();
-$last_sqlquery = lastquery_version();
-$versionto   = $version;
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/ova.class.php");
 
-$filelist = array();
-$i = 0;
-$ok = 0;
-$from = '^'.$versionfrom;
-$to = $versionto.'\.sql$';
+$ovadb = new DB();
+$ova = new OVA($ovadb); 
 
-// TODO: Add a check if db is allready up to date.
+$ova->directory_update = "./";
 
-# Recupere liste des fichiers
-$filesindir = array();
-$handle = opendir($dir);
-while (($file = readdir($handle)) !== false) 
-{
-    if (eregi ('\.sql$',$file)) $filesindir[]=$file;
-}
-sort($filesindir);
- 
-# Determine les fichiers SQL a passer
-print var_dump($filesindir);
-foreach($filesindir as $file)
-{
-	if (eregi($from,$file))
-	{
-		$filelist[]=$file;
-		// Mettre from avec valeur fin de $file
-	}
-	else if (eregi($to,$file))
-	{
-		$filelist[]=$file;
-	}
-}
-print  var_dump($filelist);
+$ova->fetch_running_version();
 
-# Boucle sur chaque fichier
-foreach($filelist as $file)
-{
-	print 'File : '.$file.'<br/>';
-	
-	$name = substr($file, 0, strlen($file) - 4);
-	$buffer = '';
-	$arraysql = Array();
-	$fp = fopen($dir.$file,"r");
-	if ($fp)
-	{
-		while (!feof ($fp))
-		{
-			$buf = fgets($fp, 4096);
-					
-			// Ajout ligne si non commentaire
-			if ((! eregi('^--',$buf)) && (! eregi('^#',$buf)))  $buffer .= $buf;
-			//          print $buf.'<br>';
+$ova->fetch_latest_version();
 
-			if (eregi(';',$buffer))
-			{
-				// Found new request
-				$arraysql[$i]=trim($buffer);
-				$i++;
-				$buffer='';
-			}
-		}
-				
-		if ($buffer) $arraysql[$i]=trim($buffer);
-		fclose($fp);
-	}
-	
-	// Loop on each request
-	foreach($arraysql as $i=>$sql)
-	{
-		if ($sql && ($i+1) > $last_sqlquery)
-		{
-			// Ajout trace sur requete (eventuellement à commenter si beaucoup de requetes)
-			print('upgrade: Request'.' '.($i+1)." sql='".$sql."\n<br/><br/>");
-			$result = db_query($sql);
-			if ( $result['result'] != FALSE ){ increase_query( ($i+1) ); }
-		}
-	}
-        
-	// update the version
-	update_db_vers();
+if ( $ova->latest_version == $ova->running_version ){
+
+	print "OK running the lastest version<br/>";
 
 }
 
-// Here we will remake all foreign keys
-$file = "fk.sql";
-$name = substr($file, 0, strlen($file) - 4);
-$buffer = '';
-$arraysql = Array();
-$fp = fopen($dir.$file,"r");
-if ($fp)
-	{
-		while (!feof ($fp))
-			{
-				$buf = fgets($fp, 4096);
-				
-				// Ajout ligne si non commentaire
-				if ((! eregi('^--',$buf)) && (! eregi('^#',$buf)))  $buffer .= $buf;
-			//          print $buf.'<br>';
-				
-				if (eregi(';',$buffer))
-					{
-						// Found new request
-						$arraysql[$i]=trim($buffer);
-						$i++;
-						$buffer='';
-					}
-			}
-		
-		if ($buffer) $arraysql[$i]=trim($buffer);
-		fclose($fp);
-	}
+$ova->fetch_latest_sql();
 
-// Loop on each request
-foreach($arraysql as $i=>$sql)
-	{
-		if ($sql)
-			{
-				// Ajout trace sur requete (eventuellement à commenter si beaucoup de requetes)
-				print('upgrade: Request'." sql='".$sql."\n<br/><br/>");
-				$result = db_query($sql,0);
-			}
-	}
+$ova->apply_latest_sql();
 
-
-// TODO: redirect to login
-
+print "Allright, everything is up to date<br/>";
+print "You can <a href='../login.php'>login</a> now";
 
 ?>
