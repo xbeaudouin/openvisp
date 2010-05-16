c7274869f846d9beb4805bbaa1dd64700bc128e6
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-16 17:49:18 +0000

    Remove old php code to be now OO.
    git-svn-id: file:///svn/openvisp@427 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/mail/edit-security.php
+++ admin/trunk/htdocs/mail/edit-security.php
@@ -18,22 +18,33 @@ require ("../config.inc.php");
 require ("../lib/functions.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
-$SESSID_USERNAME = check_user_session();
+
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+
+$SESSID_USERNAME = check_user_session ();
+
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$user_info->fetch_info($SESSID_USERNAME);
+$user_info->fetch_active_domains();
+$domain_info = new DOMAIN($ovadb);
 
 if ($_SERVER['REQUEST_METHOD'] == "GET")
 {
    $fUsername = get_get('username');
    $fDomain   = get_get('domain');
    $fAddress  = get_get('address');
-	 $domain_policy = get_domain_policy($fDomain);
 
-   if (!check_owner ($SESSID_USERNAME, $fDomain))
-   {
-      $error = 1;
-      $tMessage = $PALANG['pEdit_mailbox_domain_error'] . "<b>$fDomain</b>!</font>";
-   }
-   else
-   {
+
+	 $domain_info->fetch_by_domainname($fDomain);
+	 $user_info->check_domain_access($domain_info->data_domain['id']);
+	 $domain_info->fetch_policy();
+
+
       $resultDom = db_query ("SELECT policy.id FROM policy, domain WHERE domain.domain='$fDomain' AND domain.id=policy.domain_id");
       $rowDom = db_array ($resultDom['result']);
 
@@ -72,49 +83,14 @@ if ($_SERVER['REQUEST_METHOD'] == "GET")
       }
 
 
-			// Useless code, since only one alias address can exist.
-
-//       else
-// 				{
-// 					$result = db_query ("SELECT policy_id FROM alias WHERE address='$fAddress' AND domain='$fDomain'");
-// 					if ($result['rows'] == 1)
-// 						{
-// 							$row = db_array ($result['result']);
-// 							if ($row['policy_id'] == $rowDom['id'])
-// 								{
-// 									$result = db_query ("UPDATE alias SET policy_id='1' WHERE address='$fAddress' AND domain='$fDomain'");
-// 									if ($result['rows'] == 0)
-// 										{
-// 											$error = 1;
-// 											$tMessage = $PALANG['pEdit_security_alias_error'];
-// 										}
-// 									if ($error != 1)
-// 										{
-// 											db_log ($SESSID_USERNAME, $fDomain, "security inactive", $fAddress);
-// 										}
-// 								}
-// 							else
-// 								{
-// 									$result = db_query ("UPDATE alias SET policy_id='$rowDom[id]' WHERE address='$fAddress' AND domain='$fDomain'");
-// 									if ($result['rows'] == 0)
-// 										{
-// 											$error = 1;
-// 											$tMessage = $PALANG['pEdit_security_alias_error'];
-// 										}
-// 									if ($error != 1)
-// 										{
-// 											db_log ($SESSID_USERNAME, $fDomain, "security active", $fAddress);
-// 										}
-// 								}
-// 						}
-// 				}
 			
 			if ($error != 1)
 				{
 					header ("Location: overview.php?domain=$fDomain");
 					exit;
 				}
-	 }   
+
+
    include ("../templates/header.tpl");
    include ("../templates/mail/menu.tpl");
    include ("../templates/message.tpl");
