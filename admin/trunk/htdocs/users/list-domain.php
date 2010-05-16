36d11fece0f1bd90364c0ea285dfc6afc611fea1
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-16 17:54:09 +0000

    Add new code to be compliant with OO and ajax
    git-svn-id: file:///svn/openvisp@434 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/users/list-domain.php
+++ admin/trunk/htdocs/users/list-domain.php
@@ -18,7 +18,24 @@ require ("../lib/functions.inc.php");
 require ("../lib/accounts.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
-$SESSID_USERNAME = check_user_session();
+
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+
+$SESSID_USERNAME = check_user_session ();
+
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$user_info->fetch_info($SESSID_USERNAME);
+
+
+$user_info->check_domain_admin();
+
+// A controler ici .
+
 $list_admins = list_admins();
 
 $account_information = get_account_info($SESSID_USERNAME);
@@ -26,9 +43,125 @@ $account_quota = get_account_quota($account_information['id']);
 $total_used = get_account_used($SESSID_USERNAME,check_admin($SESSID_USERNAME));
 
 
-if ( ($_SERVER['REQUEST_METHOD'] == "GET") && (check_domain_admin($SESSID_USERNAME)) )
+if ( $_SERVER['REQUEST_METHOD'] == "GET"  )
 {
-	//   $fUsername = get_get('username');
+
+	$ajax_domain = new AJAX_YUI($ovadb);
+
+			$item_list = array(
+												 "domain" => array(
+																						"label" => $PALANG['pAdminList_domain_domain'],
+																						"parser" => "text"
+																						),
+												 "description" => array(
+																				 "label" => $PALANG['pAdminList_domain_description'],
+																				 "parser" => "text"
+																				 ),
+												 "aliases" => array(
+																						 "label" => $PALANG['pAdminList_domain_aliases'],
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+												 "quota_aliases" => array(
+																						 "label" => "",
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+												 "mails" => array(
+																						 "label" => $PALANG['pAdminList_domain_mailboxes'],
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+												 "quota_mails" => array(
+																						 "label" => "",
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+												 "backupmx" => array(
+																					 "label" => "Backup MX",
+																					 "parser" => "text"
+																					 ),
+												 "ftp" => array(
+																							"label" => $PALANG['pAdminList_domain_ftp'],
+																							"parser" => "text"
+																							),
+												 "quota_ftp" => array(
+																							"label" => "",
+																							"parser" => "text"
+																							),
+												 "web" => array(
+																				"label" => $PALANG['pAdminList_domain_http'],
+																				"parser" => "text",
+																				"sortable" => "false"
+																				),
+												 "quota_web" => array(
+																							"label" => "",
+																							"parser" => "text",
+																							"sortable" => "false"
+																							),
+												 "databases" => array(
+																							"label" => $PALANG['pAdminList_domain_sql'],
+																							"parser" => "text",
+																							"sortable" => "false"
+																							),
+												 "quota_databases" => array(
+																							"label" => "",
+																							"parser" => "text",
+																							"sortable" => "false"
+																							),
+												 "state" => array(
+																						 "label" => $PALANG['pAdminList_domain_status'],
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+												 "active" => array(
+																					 "label" => $PALANG['pAdminList_domain_active'],
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+												 "modified" => array(
+																						 "label" => $PALANG['pAdminList_domain_modified'],
+																						 "parser" => "text",
+																						 "sortable" => "false"
+																						 ),
+
+												 "delete" => array(
+																					 "label" => "delete",
+																					 "sortable" => "false",
+																					 "resizeable" => "false",
+																					 "link" => "/ajax/mail/manage_alias.php",
+																					 "url_param" => "action=delete&domainName=$fDomain",
+																					 "key_item" => "alias"
+																					 ),
+												 "edit" => array(
+																				 "label" => "",
+																				 "sortable" => "false",
+																				 "resizeable" => "false"
+																				 )
+												 );
+
+
+			$ajax_info = array(
+												 "url" => "../ajax/domain/domain_list.php",
+												 "method" => "post"
+												 );
+
+
+			$ajax_domain->ajax_info($ajax_info);
+			$ajax_domain->attr_add('root','records');
+			$ajax_domain->attr_add('sort','domain');
+			$ajax_domain->attr_add('sortdir','asc');
+			$ajax_domain->attr_add('startindex','0');
+			$ajax_domain->attr_add('maxrows','10');
+			$ajax_domain->attr_add('data_div','domain');
+			$ajax_domain->attr_add('nav_div','domain-nav');
+			$ajax_domain->item_add($item_list);
+
+			$ajax_domain->start("domain");
+			$ajax_domain->create_listener();
+
+
+
 	$fUsername = $SESSID_USERNAME;
 
    if ($fUsername != NULL)
@@ -50,6 +183,8 @@ if ( ($_SERVER['REQUEST_METHOD'] == "GET") && (check_domain_admin($SESSID_USERNA
             $domain_alias_properties[$i] = get_domain_alias_properties ($list_domains_alias[$i]['dalias']);
          }
    }
+
+	 $body_class = 'class=" yui-skin-sam"';
 
    include ("../templates/header.tpl");
    include ("../templates/users/menu.tpl");
