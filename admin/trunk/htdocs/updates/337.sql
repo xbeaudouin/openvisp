ce9b8fe4959782222560002c1358735aef5f86cd
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-16 17:52:54 +0000

    Fix sql query
    git-svn-id: file:///svn/openvisp@432 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/updates/337.sql
+++ admin/trunk/htdocs/updates/337.sql
@@ -1,9 +1,7 @@
 
 ALTER TABLE alias ADD domain_id int(11) UNSIGNED NULL DEFAULT NULL;
 
-ALTER TABLE `alias` ADD INDEX (  `domain_id` )
-
-ALTER TABLE alias ADD FOREIGN KEY ( `domain_id` ) REFERENCES `domain` (`id`) ON DELETE CASCADE;
+ALTER TABLE `alias` ADD INDEX (  `domain_id` );
 
 CREATE VIEW TMP_ALIAS AS
 SELECT domain.id as domain_id, policy.id as policy_id, alias.address, alias.domain_id as alias_domain_id
