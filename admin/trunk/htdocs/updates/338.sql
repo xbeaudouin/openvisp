e9f696c2223fef7245c6dde235e12a2d9bacd561
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-19 19:50:25 +0000

    Add change to databases
    git-svn-id: file:///svn/openvisp@457 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/updates/338.sql
+++ admin/trunk/htdocs/updates/338.sql
@@ -4,5 +4,13 @@ SET goto=TRIM(LEADING ',' FROM goto);
 
 ALTER TABLE `msgs` ADD PRIMARY KEY(`mail_id`);
 
-ALTER TABLE  `msgrcpt` ADD FOREIGN KEY (  `mail_id` ) REFERENCES  `dbopenvisp`.`msgs` (`mail_id`) ON DELETE CASCADE ;
+DROP TABLE stats_mailbox;
 
+ALTER TABLE  `ftpaccount` ADD INDEX (  `whost_id` );
+
+ALTER TABLE  `ftpaccount` CHANGE  `whost_id`  `whost_id` INT( 11 ) UNSIGNED NOT NULL
+
+ALTER TABLE  `whost_alias` DROP PRIMARY KEY ,
+ADD PRIMARY KEY (  `id` );
+
+ALTER TABLE  `whost_alias` ENGINE = INNODB;
