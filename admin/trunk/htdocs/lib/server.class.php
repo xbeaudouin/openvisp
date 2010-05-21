e030e710ef2c3b8a838286eb1a7796f92704737c
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-21 19:50:14 +0000

    Add server name inside sql query to display on which server policyd are inserted. This will be usefull to check if we have some trouble on a policyd machine.
    git-svn-id: file:///svn/openvisp@473 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/lib/server.class.php
+++ admin/trunk/htdocs/lib/server.class.php
@@ -28,13 +28,14 @@ AND server_job.active = 1";
 
 	function fetch_server_role_list($role_name){
 
-		$query = "SELECT server_job.login, server_job.password, server_job.port, server_ip.public as ip_public, server_ip.private as ip_private, server_apps.apps, server_job.instance
-FROM server_jobmodel, server_job, server_ip, server_apps
+		$query = "SELECT server.name, server_job.login, server_job.password, server_job.port, server_ip.public as ip_public, server_ip.private as ip_private, server_apps.apps, server_job.instance
+FROM server_jobmodel, server_job, server_ip, server_apps, server
 WHERE server_jobmodel.role='".$role_name."'
 AND server_jobmodel.id=server_job.server_jobmodel_id
 AND server_job.active = 1
 AND server_job.server_ip_id = server_ip.id
-AND server_job.server_apps_id = server_apps.id";
+AND server_job.server_apps_id = server_apps.id
+AND server_job.server_id = server.id";
 
 		$result = $this->db_link->sql_query($query);
 
