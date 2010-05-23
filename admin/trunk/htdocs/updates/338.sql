764839af17a3f8990413e69c5d019d7c09cf3331
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-23 18:59:59 +0000

    Change database items
    git-svn-id: file:///svn/openvisp@505 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/updates/338.sql
+++ admin/trunk/htdocs/updates/338.sql
@@ -6,15 +6,19 @@ ALTER TABLE `msgs` ADD PRIMARY KEY(`mail_id`);
 
 DROP TABLE stats_mailbox;
 
-ALTER TABLE  `ftpaccount` ADD INDEX (  `whost_id` );
+ALTER TABLE `ftpaccount` ADD INDEX ( `whost_id` );
 
-ALTER TABLE  `ftpaccount` CHANGE  `whost_id`  `whost_id` INT( 11 ) UNSIGNED NOT NULL
+ALTER TABLE `ftpaccount` CHANGE `whost_id` `whost_id` INT( 11 ) UNSIGNED NOT NULL
 
-ALTER TABLE  `whost_alias` DROP PRIMARY KEY ,
+ALTER TABLE `whost_alias` DROP PRIMARY KEY ,
 ADD PRIMARY KEY (  `id` );
 
-ALTER TABLE  `whost_alias` ENGINE = INNODB;
+ALTER TABLE `whost_alias` ENGINE = INNODB;
 
-ALTER TABLE  `server_job` ADD  `instance` VARCHAR( 50 ) NOT NULL AFTER  `password`;
+ALTER TABLE `server_job` ADD  `instance` VARCHAR( 50 ) NOT NULL AFTER  `password`;
 
-ALTER TABLE  `alias` CHANGE  `domain_id`  `domain_id` INT( 11 ) UNSIGNED NOT NULL;
+ALTER TABLE `alias` CHANGE `domain_id` `domain_id` INT( 11 ) UNSIGNED NOT NULL;
+
+ALTER TABLE `quota` CHANGE `emails` `mailboxes` INT( 11 ) NOT NULL;
+ALTER TABLE `quota` CHANGE `emails_alias` `aliases` INT( 11 ) NOT NULL;
+
