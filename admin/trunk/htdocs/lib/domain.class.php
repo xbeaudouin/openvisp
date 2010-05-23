6f030cc970ca4ccb933da8d15932114ed59011e7
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2010-05-23 14:55:33 +0000

    Remove the fetch_policy_id from default domain search. Fix sql query for fetch_policy_id
    Add a new functions :
    - domain_exist to check if a domain exist and return TRUE / FALSE.
    - create_domain to create new domain from an array of parameters
      creation include, domain, policy for amavis, associate to admin, and greylisting if allowed
    - create_domain_policy to create the amavisd configuration for a domain
    - import domain list to import from a file new domain
    - associate_domain_admin to associate a domain to an admin
    - delete domain to delete all things associated with a domain (mailboxes, amavisd, policyd)
    
    
    git-svn-id: file:///svn/openvisp@481 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/lib/domain.class.php
+++ admin/trunk/htdocs/lib/domain.class.php
@@ -22,7 +22,6 @@ class DOMAIN
 		$this->data_domain = $result['result'][0];
 		$this->domain_name = $this->data_domain['domain'];
 		$this->storhash();
-		$this->fetch_policy_id();
 		$this->fetch_quota();
 		$this->fetch_quota_status();
 	}
@@ -39,7 +38,6 @@ class DOMAIN
 		$this->data_domain = $result['result'][0];
 		$this->domain_name = $this->data_domain['domain'];
 		$this->storhash();
-    $this->fetch_policy_id();
 		$this->fetch_quota();
 		$this->fetch_quota_status();
 	}
@@ -353,8 +351,7 @@ class DOMAIN
 	//
   function fetch_policy(){
 
-    $query = "
-SELECT policy.id
+    $query = "SELECT policy.id
 FROM policy
 WHERE id=".$this->policy_id;
 
@@ -363,7 +360,310 @@ WHERE id=".$this->policy_id;
 
 
   }
+
+	function domain_exist($domain_name){
+		$query = "SELECT domain
+FROM domain
+WHERE domain='".$domain_name."'
+";
+
+    $result = $this->db_link->sql_query($query);
+		debug_info("$query");
+		if ($result['rows'] == 1){
+			return TRUE;
+		}
+		return FALSE;
+
+	}
+
+	function create_domain($domain_array){
+
+		global $CONF;
+		global $server_info;
+		global $PALANG;
+
+		$query = "INSERT INTO domain (domain,description,aliases,mailboxes,maxquota,backupmx,antivirus,vrfydomain,vrfysender,greylist,spf,created,modified,active)
+VALUES ('".$domain_array['name']."','".$domain_array['description']."','".$domain_array['mailbox_aliases']."','".$domain_array['mailboxes']."','".$domain_array['maxquota']."','".$domain_array['backupmx']."','".$domain_array['antivirus']."','".$domain_array['vrfydomain']."','".$domain_array['vrfysender']."','".$domain_array['greylisting']."','".$domain_array['spf']."',NOW(),NOW(),'".$domain_array['active']."')";
+
+		$result = $this->db_link->sql_query($query);
+
+		if ($result['rows'] != 1){
+			$array['result'] = 1;
+			$array['message'] = $PALANG['pAdminCreate_domain_result_error'] . " <b>".$domain_array['name']."</b></br/>";
+			$array['message'] .= "SQL : ".$result['sql_log']."</br/>";
+		}
+		else{
+			$array['result'] = 0;
+			$array['message'] = $PALANG['pAdminCreate_domain_result_succes'] . " <b>".$domain_array['name']."</b></br/>";
+		}
+
+		$this->fetch_by_domainname($domain_array['name']);
+		$this->associate_domain_admin();
+
+		if ( $CONF['greylisting'] == "YES" ){
+ 
+			if ( $server_info->check_server_role_exist('policy') ){
+
+				$server_info->fetch_server_role_list('policy');
+
+				for ( $i=0; $i < sizeof($server_info->list_server_role); $i++){
+
+					$db_name = $server_info->list_server_role[$i]['instance'];
+					$db_user = $server_info->list_server_role[$i]['login'];
+					$db_pass = $server_info->list_server_role[$i]['password'];
+					$db_port = $server_info->list_server_role[$i]['port'];
+					$db_type = "mysql";
+					$db_host = $server_info->list_server_role[$i]['ip_public'];
+					if ( $server_info->list_server_role[$i]['ip_private'] != "") {$db_host = $server_info->list_server_role[$i]['ip_private'];}
+
+					$policydb = new DB($db_name, $db_type, $db_host, $db_user, $db_pass, $db_port);
+					$policy_info = new POLICYD($policydb);
+					
+
+					$policy_info_rcpt = $policy_info->add_new_domain_policy($this->data_domain['domain']);
+					if ( $policy_info_rcpt['result'] != 1){
+						$array['message'] .= $policy_info_rcpt['message'];
+					}
+					$array['result'] = $policy_info_rcpt['result'];
+		
+		
+					$policy_info_helo = $policy_info->add_forbidden_helo($this->data_domain['domain']);
+					if ( $policy_info_helo['result'] != 1){
+						$array['message'] .= $policy_info_helo['message'];
+					}
+					$array['result'] += $policy_info_helo['result'];
+
+				}
+
+			}
+
+		}
+
+		return $array;
+
+	}
+
+	function create_domain_policy($policy_array){
+		
+		global $CONF;
+
+		debug_info("DB : " . $query ." // " . $result['rows']);		
+		$query = "INSERT INTO policy (domain_id, virus_lover, spam_lover, banned_files_lover, bad_header_lover, bypass_virus_checks, bypass_spam_checks, bypass_banned_checks, bypass_header_checks, spam_modifies_subj, virus_quarantine_to, spam_quarantine_to, banned_quarantine_to, bad_header_quarantine_to, spam_tag_level, spam_tag2_level, spam_kill_level, spam_dsn_cutoff_level, addr_extension_virus, addr_extension_spam, addr_extension_banned, addr_extension_bad_header, warnvirusrecip, warnbannedrecip, warnbadhrecip, newvirus_admin, virus_admin, banned_admin, bad_header_admin, spam_admin, spam_subject_tag, spam_subject_tag2, message_size_limit, banned_rulenames)
+VALUES ('".$this->data_domain['id']."','".$policy_array['virus_lover']."','".$policy_array['spam_lover']."','".$policy_array['banned_files_lover']."','".$policy_array['bad_header_lover']."','".$policy_array['antivirus']."','".$policy_array['spam_lover']."','".$policy_array['bypass_banned_checks']."','".$policy_array['bypass_header_checks']."','Y','".$CONF['virus_quarantine_to']."','".$CONF['spam_quarantine_to']."','".$CONF['banned_quarantine_to']."','','".$CONF['sa_tag_level']."','".$CONF['sa_tag2_level'] ."','".$CONF['sa_kill_level'] ."','','','','','','N','N','N','','','','','','".$CONF['spam_subject_tag']."','".$CONF['spam_subject_tag2']."','','')";
+
+		$result = $this->db_link->sql_query($query);
+
+		debug_info("DB : " . $query ." // " . $result['rows']);
+
+		if ($result['rows'] != 1){
+			$array['result'] = 1;
+			$array['message'] = $PALANG['pAdminCreate_domain_result_error2'] . " <b>".$array['name']."</b></br/>";
+			$array['message'] .= "SQL : ".$result['sql_log']."</br/>";
+		}
+		else{
+			$array['result'] = 0;
+			$array['message'] = $PALANG['pAdminCreate_domain_result_succes'] . " <b>".$array['name']."</b></br/>";
+		}
+
+		debug_info("DB : " . $query ." // " . $result['rows']);
+
+		return $array;
+
+	}
+
+  //
+	// import_domains_list
+	// Action: import a list of domain
+	// Call: import_domains_list ($file)
+	//
+  function import_domains_list($new_domains){
+
+		global $CONF;
+		global $PALANG;
+
+		$array['message'] = "";
+
+		foreach ($new_domains as $line_num => $line) {
+			$info = explode(";", $line);
+			$newDomain['name'] = chop($info[0]);
+			// ($domain_info->list_mail_aliases[$i]['active'] == 0) ? $PALANG['NO'] : $PALANG['YES'],
+
+			$newDomain['backupmx'] = ( isset($info[1]) ) ? $info[1] : 0;
+			$newDomain['mailboxes'] = ( isset($info[2]) ) ? $info[2] : $CONF['mailboxes'];
+			$newDomain['mailbox_aliases'] = ( isset($info[3]) ) ? $info[3] : $CONF['aliases'];
+			$newDomain['maxquota'] = ( isset($info[4]) ) ? $info[4]  : $CONF['maxquota'];
+			$newDomain['description'] = ( isset($info[5]) ) ? $info[5] : "";
+			$newDomain['antivirus'] = ( isset($info[6]) ) ? $info[6]  : 0;
+			$newDomain['spamass'] = ( isset($info[7]) ) ? $info[7] : 0;
+			$newDomain['vrfysender'] = ( isset($info[8]) ) ? $info[8] : 0;
+			$newDomain['vrfydomain'] = ( isset($info[9]) ) ? $info[9] : 0;
+			$newDomain['greylisting'] = ( isset($info[10]) ) ? $info[10] : 0;
+			$newDomain['spf'] = ( isset($info[11]) ) ? $info[11] : 0;
+			$newDomain['active'] = ( isset($info[12]) ) ? $info[12] : 0;
+			$newDomain['default_aliases'] = ( isset($info[13]) ) ? $info[13] : 0;
+
+			if ( $this->domain_exist($newDomain['name']) ){
+				$array['result'] = 1;
+				$array['message'] = $newDomain['name'] . " : " . $PALANG['pAdminCreate_domain_domain_text_error'] . "<br/>";
+				return $array;
+			}
+			else {
+
+				if ($newDomain['backupmx'] == "1")
+					{
+						$newDomain['mailbox_aliases'] = 0;
+						$newDomain['mailboxes'] = 0;
+						$newDomain['maxquota'] = 0;
+					}
+
+				$result = $this->create_domain($newDomain);
+
+				$array['message'] .= $result['message'];
+
+				if ($result['result'] > 0){
+					$array['result'] = $result['result'];
+					return $array;
+				}
+
+				if ($newDomain['antivirus'] == 0 ){
+					$amavis['antivirus'] = 'N';
+					$amavis['virus_lover'] = 'Y';
+					$amavis['banned_files_lover'] = 'Y';
+					$amavis['bad_header_lover'] = 'Y';
+					$amavis['bypass_banned_checks'] = 'Y';
+					$amavis['bypass_header_checks'] = 'Y';
+				}
+				else{ 
+					$amavis['antivirus'] = 'Y';
+					$amavis['virus_lover'] = 'N';
+					$amavis['banned_files_lover'] = 'N';
+					$amavis['bad_header_lover'] = 'N';
+					$amavis['bypass_banned_checks'] = 'N';
+					$amavis['bypass_header_checks'] = 'N';
+				}
+
+				$amavis['spam_lover'] = ( $newDomain['spamass'] == 0 ) ? 'Y' : 'N';
+
+
+				$result = $this->create_domain_policy($amavis);
+				$array['message'] .= $result['message'];
+
+				if ($result['result'] > 0){
+					$array['result'] = $result['result'];
+					return $array;
+				}
+				else
+					{
+						$this->fetch_policy_id();
+
+						if ($newDomain['backupmx'] == 1)
+							{
+								$domain_addr = "@" . $newDomain['name'];
+								$mail_info->add_mail_alias($domain_addr,$domain_addr);
+							}
+						if ( $newDomain['default_aliases'] == "1")
+							{
+								foreach ($CONF['default_aliases'] as $address=>$goto)
+									{
+										$address = $address . "@" . $newDomain['name'];
+										$mail_info->add_mail_alias($address, $goto);
+									}
+							}
+						$tMessage = $PALANG['pAdminCreate_domain_result_succes'] . "<br/>(".$newDomain['name'].")<br/>";
+					}
+			}
+			print $tMessage."<br />";
+		}
+
+		return $array;
+
+
+  }
   
+	function associate_domain_admin(){
+
+		global $PALANG;
+		global $user_info;
+
+		$query = "INSERT INTO domain_admins (accounts_id, domain_id, active)
+VALUES (".$user_info->data['id'].", ".$this->data_domain['id']." , 1 )
+";
+
+    $result = $this->db_link->sql_query($query);
+
+		$array['message'] = "";
+
+		if ($result['result'] > 0){
+			$array['message'] = $PALANG['pAssociate_domain_admin_failed'];
+			$array['message'] .= $result['message'];
+		}
+		$array['result'] = $result['result'];
+
+		return $array;
+	
+	}
+  
+	function delete_domain(){
+
+		global $PALANG;
+		global $CONF;
+		global $server_info;
+		global $user_info;
+
+		$array['message'] = "";
+
+		debug_info("Start delete domain ".$this->data_domain['domain']);
+
+		if ( $CONF['greylisting'] == "YES" ){
+ 
+			debug_info("Greylisting ON ");
+
+			if ( $server_info->check_server_role_exist('policy') ){
+
+				$server_info->fetch_server_role_list('policy');
+
+				for ( $i=0; $i < sizeof($server_info->list_server_role); $i++){
+
+					$db_name = $server_info->list_server_role[$i]['instance'];
+					$db_user = $server_info->list_server_role[$i]['login'];
+					$db_pass = $server_info->list_server_role[$i]['password'];
+					$db_port = $server_info->list_server_role[$i]['port'];
+					$db_type = "mysql";
+					$db_host = $server_info->list_server_role[$i]['ip_public'];
+					if ( $server_info->list_server_role[$i]['ip_private'] != "") {$db_host = $server_info->list_server_role[$i]['ip_private'];}
+
+					$policydb = new DB($db_name, $db_type, $db_host, $db_user, $db_pass, $db_port);
+					$policy_info = new POLICYD($policydb);
+
+					$policy_info_rcpt = $policy_info->remove_domain_policy($this->data_domain['domain']);
+					if ( $policy_info_rcpt['result'] != 1){
+						$array['message'] .= $policy_info_rcpt['message'];
+					}
+					$array['result'] = $policy_info_rcpt['result'];
+		
+		
+					$policy_info_helo = $policy_info->remove_forbidden_helo($this->data_domain['domain']);
+					if ( $policy_info_helo['result'] != 1){
+						$array['message'] .= $policy_info_helo['message'];
+					}
+					$array['result'] += $policy_info_helo['result'];
+
+				}
+
+			}
+
+		}
+
+
+		$this->policy_message = $array['message'];
+
+		$query = "DELETE FROM domain WHERE id=".$this->data_domain['id'];
+    $this->sql_result = $this->db_link->sql_query($query,2);
+
+
+
+	}
+
 
 }
 
