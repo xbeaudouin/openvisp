4c96ecae4b6b885f2bf5c744fe08c49cca188891
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-02-23 17:22:58 +0000

    add YUI 3.5.0pr2
    git-svn-id: file:///svn/openvisp@569 bed1d25c-e804-df11-9dd7-000c2933e665

---
+++ admin/trunk/htdocs/lib/yui3.5.0pr2/selector/selector-debug.js
@@ -0,0 +1,12 @@
+/*
+YUI 3.5.0pr2 (build 4560)
+Copyright 2011 Yahoo! Inc. All rights reserved.
+Licensed under the BSD License.
+http://yuilibrary.com/license/
+*/
+YUI.add('selector', function(Y) {
+
+
+
+
+}, '3.5.0pr2' ,{requires:['selector-native']});
---
+++ admin/trunk/htdocs/lib/yui3.5.0pr2/selector/selector-min.js
@@ -0,0 +1,7 @@
+/*
+YUI 3.5.0pr2 (build 4560)
+Copyright 2011 Yahoo! Inc. All rights reserved.
+Licensed under the BSD License.
+http://yuilibrary.com/license/
+*/
+YUI.add("selector",function(a){},"3.5.0pr2",{requires:["selector-native"]});
