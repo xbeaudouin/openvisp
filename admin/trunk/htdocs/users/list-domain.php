6a4073688b23753bf78ffc841e7102a58af91661
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-03-04 15:26:02 +0000

    Upgrade call from yui2 to yui3 remove the old menu reindent code
    git-svn-id: file:///svn/openvisp@590 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/users/list-domain.php
+++ admin/trunk/htdocs/users/list-domain.php
@@ -45,144 +45,124 @@ $user_info->fetch_quota_status();
 //$total_used = get_account_used($SESSID_USERNAME,check_admin($SESSID_USERNAME));
 
 
-if ( $_SERVER['REQUEST_METHOD'] == "GET"  )
-{
+if ( $_SERVER['REQUEST_METHOD'] == "GET"  ){
 
-	$ajax_domain = new AJAX_YUI($ovadb);
+  $ajax_domain = new AJAX_YUI($ovadb);
 
-			$item_list = array(
-												 "domain" => array(
-																						"label" => $PALANG['pAdminList_domain_domain'],
-																						"parser" => "text"
-																						),
-												 "description" => array(
-																				 "label" => $PALANG['pAdminList_domain_description'],
-																				 "parser" => "text"
-																				 ),
-												 "aliases" => array(
-																						 "label" => $PALANG['pAdminList_domain_aliases'],
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-												 "quota_aliases" => array(
-																						 "label" => "",
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-												 "mails" => array(
-																						 "label" => $PALANG['pAdminList_domain_mailboxes'],
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-												 "quota_mails" => array(
-																						 "label" => "",
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-												 "backupmx" => array(
-																					 "label" => "Backup MX",
-																					 "parser" => "text"
-																					 ),
-												 "ftp" => array(
-																							"label" => $PALANG['pAdminList_domain_ftp'],
-																							"parser" => "text"
-																							),
-												 "quota_ftp" => array(
-																							"label" => "",
-																							"parser" => "text"
-																							),
-												 "web" => array(
-																				"label" => $PALANG['pAdminList_domain_http'],
-																				"parser" => "text",
-																				"sortable" => "false"
-																				),
-												 "quota_web" => array(
-																							"label" => "",
-																							"parser" => "text",
-																							"sortable" => "false"
-																							),
-												 "databases" => array(
-																							"label" => $PALANG['pAdminList_domain_sql'],
-																							"parser" => "text",
-																							"sortable" => "false"
-																							),
-												 "quota_databases" => array(
-																							"label" => "",
-																							"parser" => "text",
-																							"sortable" => "false"
-																							),
-												 "state" => array(
-																						 "label" => $PALANG['pAdminList_domain_status'],
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-												 "active" => array(
-																					 "label" => $PALANG['pAdminList_domain_active'],
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-												 "paid" => array(
-																				 "label" => $PALANG['pGeneric_paid'],
-																				 "parser" => "text",
-																				 "sortable" => "false"
-																				 ),
-												 "modified" => array(
-																						 "label" => $PALANG['pAdminList_domain_modified'],
-																						 "parser" => "text",
-																						 "sortable" => "false"
-																						 ),
-
-												 "delete" => array(
-																					 "label" => "delete",
-																					 "sortable" => "false",
-																					 "resizeable" => "false",
-																					 "link" => "/ajax/domain/manage_domain.php",
-																					 "url_param" => "action=delete",
-																					 "key_item" => "domain"
-																					 ),
-												 "edit" => array(
-																				 "label" => "",
-																				 "sortable" => "false",
-																				 "resizeable" => "false"
-																				 )
-												 );
+  $item_list = array(
+    "domain" => array(
+      "label" => "'".$PALANG['pAdminList_domain_domain']."'",
+      "parser" => "'"."text"."'"
+    ),
+    "description" => array(
+      "label" => "'".$PALANG['pAdminList_domain_description']."'",
+      "parser" => "'"."text"."'"
+    ),
+    "aliases_w_quota" => array(
+       "label" => "'".$PALANG['pAdminList_domain_aliases']."'",
+       "parser" => "'"."text"."'",
+       "sortable" => "false",
+        "allowHTML" => "true"
+    ),
+    "mails_w_quota" => array(
+       "label" => "'".$PALANG['pAdminList_domain_mailboxes']."'",
+       "parser" => "'"."text"."'",
+       "sortable" => "false",
+       "allowHTML" => "true"
+    ),
+    "backupmx" => array(
+      "label" => "'"."Backup MX"."'",
+      "parser" => "'"."text"."'"
+    ),
+    "ftp_w_quota" => array(
+      "label" => "'".$PALANG['pAdminList_domain_ftp']."'",
+      "parser" => "'"."text"."'",
+      "allowHTML" => "true"
+    ),
+    "web_w_quota" => array(
+      "label" => "'".$PALANG['pAdminList_domain_http']."'",
+      "parser" => "'"."text"."'",
+      "sortable" => "false",
+      "allowHTML" => "true"
+    ),
+    "databases_w_quota" => array(
+      "label" => "'".$PALANG['pAdminList_domain_sql']."'",
+      "parser" => "'"."text"."'",
+      "sortable" => "false",
+      "allowHTML" => "true"
+    ),
+    "state" => array(
+      "label" => "'".$PALANG['pAdminList_domain_status']."'",
+      "parser" => "'"."text"."'",
+      "sortable" => "false"
+    ),
+    "active" => array(
+      "label" => "'".$PALANG['pAdminList_domain_active']."'",
+      "parser" => "'"."text"."'",
+      "sortable" => "false"
+    ),
+    "paid" => array(
+      "label" => "'".$PALANG['pGeneric_paid']."'",
+      "parser" => "'"."text"."'",
+      "sortable" => "false"
+    ),
+    "modified" => array(
+      "label" => "'".$PALANG['pAdminList_domain_modified']."'",
+      "parser" => "'"."text"."'",
+      "sortable" => "false"
+    ),
+    "delete" => array(
+      "label" => "'"."delete"."'",
+      "sortable" => "false",
+      "resizeable" => "false",
+      "link" => "'"."/ajax/domain/manage_domain.php"."'",
+      "url_param" => "'"."action=delete"."'",
+      "key_item" => "'"."domain"."'"
+    ),
+    "edit" => array(
+      "label" => "''",
+      "sortable" => "false",
+      "resizeable" => "false",
+      "allowHTML" => "true"
+    ),
+  );
 
 
-			$ajax_info = array(
-												 "url" => "../ajax/domain/domain_list.php",
-												 "method" => "post"
-												 );
+  $ajax_info = array(
+    "url" => "../ajax/domain/domain_list.php",
+    "method" => "get",
+    "table_summary" => "List of domains",
+    "table_caption" => "List of domains",
+    "params" => array (
+      "startIndex" => "0",
+      "results" => "100"
+    )
+  );
 
 
-			$ajax_domain->ajax_info($ajax_info);
-			$ajax_domain->attr_add('root','records');
-			$ajax_domain->attr_add('sort','domain');
-			$ajax_domain->attr_add('sortdir','asc');
-			$ajax_domain->attr_add('startindex','0');
-			$ajax_domain->attr_add('maxrows','10');
-			$ajax_domain->attr_add('data_div','domain');
-			$ajax_domain->attr_add('nav_div','domain-nav');
-			$ajax_domain->item_add($item_list);
+  $ajax_domain->ajax_info($ajax_info);
+  $ajax_domain->attr_add('root','records');
+  $ajax_domain->attr_add('sort','domain');
+  $ajax_domain->attr_add('sortdir','asc');
+  $ajax_domain->attr_add('startindex','0');
+  $ajax_domain->attr_add('maxrows','10');
+  $ajax_domain->attr_add('data_div','domain');
+  $ajax_domain->attr_add('nav_div','domain-nav');
+  $ajax_domain->item_add($item_list);
 
-			$ajax_domain->start("domain");
-			$ajax_domain->create_listener();
+
+  $ajax_domain->start("domain");
+  //$ajax_domain->create_listener();
+  $ajax_domain->create_datasource();
+  $ajax_domain->create_datatable();
 
 
 
-	$fUsername = $SESSID_USERNAME;
+  $fUsername = $SESSID_USERNAME;
 
    if ($fUsername != NULL)
    {
 
-      $list_domains = list_domains_for_admin ($fUsername);
-      if ($list_domains != 0)
-      {
-         for ($i = 0; $i < sizeof ($list_domains); $i++)
-         {
-            $domain_properties[$i] = get_domain_properties ($list_domains[$i]);
-						//	          $domain_policy[$i] = get_domain_policy ($list_domains[$i]);
-         }
-      }
       $list_domains_alias = list_domains_alias_for_admin ($fUsername);
       if ((is_array ($list_domains_alias) and sizeof ($list_domains_alias) > 0))
          for ($i = 0; $i < sizeof ($list_domains_alias); $i++)
@@ -191,10 +171,10 @@ if ( $_SERVER['REQUEST_METHOD'] == "GET"  )
          }
    }
 
-	 $body_class = 'class=" yui-skin-sam"';
+   $body_class = 'class="yui3-skin-sam"';
 
    include ("../templates/header.tpl");
-   include ("../templates/users/menu.tpl");
+//   include ("../templates/users/menu.tpl");
    include ("../templates/users/list-domain.tpl");
    include ("../templates/footer.tpl");
 }
@@ -211,7 +191,7 @@ if ( ($_SERVER['REQUEST_METHOD'] == "POST") && (check_domain_admin($SESSID_USERN
         for ($i = 0; $i < sizeof ($list_domains); $i++)
         {
            $domain_properties[$i] = get_domain_properties ($list_domains[$i]);
-	 $domain_policy[$i] = get_domain_policy ($list_domains[$i]);
+   $domain_policy[$i] = get_domain_policy ($list_domains[$i]);
         }
      }
      $list_domains_alias = list_domains_alias_for_admin ($fUsername);
