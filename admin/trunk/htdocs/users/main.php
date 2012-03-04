0cdd6faf72c14da1d3f21e0f5eaa7add733486ae
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-03-04 15:29:19 +0000

    Add some requirement to display information in the new menu
    git-svn-id: file:///svn/openvisp@592 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/users/main.php
+++ admin/trunk/htdocs/users/main.php
@@ -18,9 +18,28 @@ require ("../lib/hosting.inc.php");
 require ("../lib/accounts.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+
+
 $SESSID_USERNAME = check_user_session ();
 
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$user_info->fetch_info($SESSID_USERNAME);
+$user_info->fetch_active_domains();
+$domain_info = new DOMAIN($ovadb);
+
+$user_info->fetch_quota_status();
+$user_info->fetch_domains();
+
 $body_class = 'class="yui3-skin-sam"';
+
+$ajax_domain = new AJAX_YUI($ovadb);
+$ajax_domain->start("domain");
 
 if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
