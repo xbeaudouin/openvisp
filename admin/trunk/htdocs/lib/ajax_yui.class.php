a3d6ac89baac02448d762da73e22c766c957cc75
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-03-04 15:15:38 +0000

    Change indentation of the file. Add new lib to class when call end method. Add the search form at the top of the page to select working domain. Fix the limit, record start, record limit in ajax call.
    git-svn-id: file:///svn/openvisp@584 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/lib/ajax_yui.class.php
+++ admin/trunk/htdocs/lib/ajax_yui.class.php
@@ -3,13 +3,12 @@
 class AJAX_YUI
 {
 
-	protected $db_link;
+  protected $db_link;
 
-	function __construct ($db_link)
-	{
-		$this->db_link = $db_link;
-	}
-	
+  function __construct ($db_link){
+    $this->db_link = $db_link;
+  }
+
 
   function add_search_form($item_list){
     $this->search_form=$item_list;
@@ -18,20 +17,20 @@ class AJAX_YUI
   function add_function($function_name, $function_content){
     $this->js_function .= 'var '.$function_name.' = '.$function_content."\n\n"; 
   }
-	
-	function generate_search_form($size){
-	  foreach ($this->search_form as $item => $value) {
-	    //$value['name'];
-	    print '
-	      <td width="'.$size.'">
-	        <div id="autocomplete_'.$value['name'].'">
-	        <label for="dt_input_'.$value['name'].'">'.$value['form_inputname'].'</label><input id="dt_input_'.$value['name'].'" type="text" value="">
-	        <div id="dt_ac_'.$value['name'].'_container"></div>
-	        </div>
-	      </td>';
-	  }
 
-	}
+  function generate_search_form($size){
+    foreach ($this->search_form as $item => $value) {
+      //$value['name'];
+      print '
+        <td width="'.$size.'">
+          <div id="autocomplete_'.$value['name'].'">
+          <label for="dt_input_'.$value['name'].'">'.$value['form_inputname'].'</label><input id="dt_input_'.$value['name'].'" type="text" value="">
+          <div id="dt_ac_'.$value['name'].'_container"></div>
+          </div>
+        </td>';
+    }
+
+  }
 
 /*
   Global function not specific to yui but to ova project.
@@ -51,7 +50,7 @@ class AJAX_YUI
 
 
   function start ($primary_key = NULL){
-		$this->js_function ="";
+    $this->js_function ="";
     $this->name=$primary_key;
     $this->buffer = " ";
     $this->cnt_lib = 0;
@@ -59,9 +58,9 @@ class AJAX_YUI
     $this->load_lib = "<script>";
 
 
-		// Make YUI load locally the lib and not from yahoo web site
+    // Make YUI load locally the lib and not from yahoo web site
 
-		$this->load_lib .= '
+    $this->load_lib .= '
 
 YUI_config = {
     groups: {
@@ -74,7 +73,7 @@ YUI_config = {
             modules: {
                 \'gallery-paginator\': {
                     path     : \'gallery-paginator/gallery-paginator.js\',
-                    skinnable: true           // Optional.
+                    skinnable: true
                 }
             }
         }
@@ -106,18 +105,63 @@ YUI().use(";
 
 
   function end(){
+    $this->load_yui_lib("datasource-io");
+    $this->load_yui_lib("autocomplete");
+    $this->load_yui_lib("event");
+    $this->load_yui_lib("node");
+
     print $this->load_lib;
     print ", function (Y) {\n";
 
-		print "";
+    print "";
 
     print $this->buffer."\n";
 
-		print $this->js_function;
+    print "
+
+    var ACDomain = new Y.AutoComplete({
+      inputNode: '#DomainSearch',
+      resultListLocator: 'records',
+      resultTextLocator: 'domain',
+      source : '".$_SESSION['absoluteuri']."ajax/domain/domain_search.php?domain={query}',
+      render   : true
+    });
+
+    var clean = function (e){
+      if ( e.target.get(\"value\") == \"Search\" ){
+        e.target.set(\"value\", \"\");
+      }
+    };
+
+    var fill = function (e){
+      if ( e.target.get(\"value\") == \"\" ){
+        e.target.set(\"value\", \"Search\");
+      }
+    };
+
+    var select = function (e){
+      CurDomain.setContent(\"Current Domain \" + e.target.get(\"value\"));
+      DS_searchDomain.sendRequest({
+        request:\"wdomain=\" + e.target.get(\"value\")
+      });
+    }
+
+    var CurDomain = Y.one('#CurDomain'),
+        IDomainSearch = Y.one('#DomainSearch');
+
+    IDomainSearch.on('focus', clean);
+    IDomainSearch.on('blur', fill);
+    ACDomain.after('select', select);
+
+    var DS_searchDomain = new Y.DataSource.IO({source:'".$_SESSION['absoluteuri']."/ajax/manage_session.php?', ioConfig: { method: 'POST'}});
+
+";
+
+    print $this->js_function;
 
     print "})\n";
 
-		//		print "alert('NGO' + totalRecords);";
+    //		print "alert('NGO' + totalRecords);";
 
     print "</script>\n";
   }
@@ -125,6 +169,23 @@ YUI().use(";
 /*
   YUI 3 lib
 */
+
+
+  /*
+    Function create_search_field_autocomplete
+    function to attache a yui object to a search field
+  */
+
+  function create_search_field_autocomplete($options){
+    $this->load_yui_lib("datasource-io");
+    $this->load_yui_lib("autocomplete");
+    $this->load_yui_lib("event");
+    $this->load_yui_lib("node");
+
+    $this->buffer .= "\n\n";
+
+
+  }
 
   /*
     Function create_datasource
@@ -135,24 +196,24 @@ YUI().use(";
   function create_datasource(){
     $this->load_yui_lib("datasource-io");
     $this->load_yui_lib("datasource-jsonschema");
-		$this->load_yui_lib("gallery-paginator");
+    $this->load_yui_lib("gallery-paginator");
 
     $this->buffer .= "\n\n";
 
-		$add_request = "";
-		if ( isset($this->ajax_info['params']) && gettype($this->ajax_info['params']) == "array" ){
-			foreach ( $this->ajax_info['params'] as $key => $value ){
-				if ( $add_request != "" ){ $add_request .= "&";}
-				$add_request .= "$key=$value";
-			}
-		}
+    $add_request = "";
+    if ( isset($this->ajax_info['params']) && gettype($this->ajax_info['params']) == "array" ){
+      foreach ( $this->ajax_info['params'] as $key => $value ){
+        if ( $add_request != "" ){ $add_request .= "&";}
+        $add_request .= "$key=$value";
+      }
+    }
 
 
-		$this->buffer .= "
+    $this->buffer .= "
 
   function sendRequest_".$this->name."(){
     table_".$this->name.".datasource.load({
-      request: \"startIndex=\"+".$this->name."_pg.getStartIndex() + \"&resultCount=\" + ".$this->name."_pg.getRowsPerPage() + \"&".$add_request."\"
+      request: \"".$add_request."&startIndex=\"+".$this->name."_pg.getStartIndex() + \"&results=\" + ".$this->name."_pg.getRowsPerPage()
     })
 
   }
@@ -170,40 +231,40 @@ YUI().use(";
       },
       resultFields: [\n";
 
-	 	    $boucle=1;
-	 	    foreach ($this->item_list as $item => $attributes) {
+    $boucle=1;
+    foreach ($this->item_list as $item => $attributes) {
 
-					if ( preg_match("/children.*/", $item) ){
-						$item = "children";
-					}
+      if ( preg_match("/children.*/", $item) ){
+        $item = "children";
+      }
 
 
-					switch ($item){
+      switch ($item){
 
-					case "children":
-						$total_child = sizeof($attributes);
-						$boucle_child = 0;
-						foreach ($attributes as $child_array){
-							if ( is_array($child_array) ){
-								$this->buffer .= '  { key:"'.$child_array['key'].'"';
-								if ( ! empty($child_array['parser']) ) { $this->buffer .= ', parser:'.$child_array['parser']; }
-								$this->buffer .= '} ';
-								
-								if ( $boucle_child < $total_child ) { $this->buffer .= ","; }
-								$boucle_child++;
-							}
-					}						
-						break;
+        case "children":
+          $total_child = sizeof($attributes);
+          $boucle_child = 0;
+          foreach ($attributes as $child_array){
+            if ( is_array($child_array) ){
+              $this->buffer .= '  { key:"'.$child_array['key'].'"';
+              if ( ! empty($child_array['parser']) ) { $this->buffer .= ', parser:'.$child_array['parser']; }
+              $this->buffer .= '} ';
 
-					default:
-						$this->buffer .= '	                {key:"'.$item.'"';
-						if ( isset($attributes['parser']) ) { $this->buffer .= ', parser:'.$attributes['parser'];}
-						$this->buffer .= '}';
-						
-	 	        if ( $boucle < sizeof($this->item_list)){
-	 	          $this->buffer .= ",\n";
-	 	        }
-					
+              if ( $boucle_child < $total_child ) { $this->buffer .= ","; }
+              $boucle_child++;
+            }
+          }
+          break;
+
+         default:
+            $this->buffer .= '                  {key:"'.$item.'"';
+            if ( isset($attributes['parser']) ) { $this->buffer .= ', parser:'.$attributes['parser'];}
+            $this->buffer .= '}';
+
+            if ( $boucle < sizeof($this->item_list)){
+              $this->buffer .= ",\n";
+            }
+
 
 	 	      }
 	 	      $boucle++;
@@ -237,8 +298,8 @@ YUI().use(";
 
 	var ".$this->name."_pg = new Y.Paginator(
 	{
-		rowsPerPage: ".$this->info['maxrows'].",
-		rowsPerPageOptions: [1,2,5,10,25,50],
+		rowsPerPage: ".$this->ajax_info['params']['results'].",
+		rowsPerPageOptions: [1,2,5,10,25,50,100],
 		template: '{FirstPageLink} {PreviousPageLink} {PageLinks} {NextPageLink} {LastPageLink} <span class=\"pg-rpp-label\">Rows per page:</span> {RowsPerPageDropdown}'
 	});
 	".$this->name."_pg.render('#".$this->name."-nav');
@@ -281,119 +342,115 @@ YUI().use(";
     $this->buffer .="  var table_".$this->name." = new Y.DataTable({\n
     columns: [\n";
 
-        $boucle=1;
-        foreach ($this->item_list as $item => $attributes){
+    $boucle=1;
+    foreach ($this->item_list as $item => $attributes){
 
-          if ( !isset($attributes['display']) ) { $attributes['display'] = TRUE;}
-          
-          if ( $attributes['display'] == TRUE ){
+      if ( !isset($attributes['display']) ) { $attributes['display'] = TRUE;}
+      
+      if ( $attributes['display'] == TRUE ){
 
-            if ( preg_match("/children.*/", $item) ){
-              $item = "children";
-            }
-
-            switch ($item){
-              case "children":
-                $this->buffer .= '      { ';
-                if ( isset($attributes['label']) )
-                { $this->buffer .= 'label:'.$attributes['label'].','; }
-                $this->buffer .= " children:\n";
-                $this->buffer .= "        [\n";
-
-                $total_child = sizeof($attributes) - 3;
-                $boucle_child = 0;
-                foreach ($attributes as $child_array){
-                  if ( is_array($child_array) ){
-                    $this->buffer .= '          { key:"'.$child_array['key'].'"';
-
-
-                    if ( ! empty($child_array['sortable']) ){
-                      $this->buffer .= ', sortable:'.$child_array['sortable'];
-                    }
-
-                    if ( ! empty($child_array['resizeable']) ){
-                      $this->buffer .= ', resizeable:'.$child_array['resizeable'];
-                    }
-
-                    if ( ! empty($child_array['label']) ) {
-                      //$child_array['label'];
-                      $this->buffer .= ', label:'.$child_array['label'];
-                    }
-
-                    if ( ! empty($child_array['formater']) ) {
-                      //$child_array['label'];
-                      $this->buffer .= ', formatter:'.$child_array['label'];
-                    }
-
-                    $this->buffer .= "}";
-
-                    if ( $boucle_child < $total_child ) { $this->buffer .= ",\n"; }
-                    $boucle_child++;
-
-                  }
-                }
-
-                $this->buffer .= "\n        ]\n";
-                $this->buffer .= "      }";
-                if ( $boucle < sizeof($this->item_list) -1 ){
-                  $this->buffer .= ",\n";
-                }
-                else {$this->buffer .= ",";}
-                
-                
-                break;
-
-                default:
-                  if ( empty($attributes['label']) ) { $attributes['label']="";}
-                  
-                  $this->buffer .= "      {\n";
-									$this->buffer .= "key:\"$item\",\n";
-										//, label:"'.$attributes['label'].'"';
-									foreach ($attributes as $att_key => $att_value) {
-										$this->buffer .= "$att_key:$att_value,\n";
-									}
-                  $this->buffer .= "      }\n";
-                  if ( $boucle < sizeof($this->item_list) ){
-                    $this->buffer .= ",\n";
-                  }
-
-            }
-          }
-          $boucle++;
+        if ( preg_match("/children.*/", $item) ){
+          $item = "children";
         }
+
+        switch ($item){
+          case "children":
+            $this->buffer .= '      { ';
+            if ( isset($attributes['label']) )
+            { $this->buffer .= 'label:'.$attributes['label'].','; }
+            $this->buffer .= " children:\n";
+            $this->buffer .= "        [\n";
+
+            $total_child = sizeof($attributes) - 3;
+            $boucle_child = 0;
+            foreach ($attributes as $child_array){
+              if ( is_array($child_array) ){
+                $this->buffer .= '          { key:"'.$child_array['key'].'"';
+
+
+                if ( ! empty($child_array['sortable']) ){
+                  $this->buffer .= ', sortable:'.$child_array['sortable'];
+                }
+
+                if ( ! empty($child_array['resizeable']) ){
+                  $this->buffer .= ', resizeable:'.$child_array['resizeable'];
+                }
+
+                if ( ! empty($child_array['label']) ) {
+                  //$child_array['label'];
+                  $this->buffer .= ', label:'.$child_array['label'];
+                }
+
+                if ( ! empty($child_array['formater']) ) {
+                  //$child_array['label'];
+                  $this->buffer .= ', formatter:'.$child_array['label'];
+                }
+
+                $this->buffer .= "}";
+
+                if ( $boucle_child < $total_child ) { $this->buffer .= ",\n"; }
+                $boucle_child++;
+
+              }
+            }
+
+            $this->buffer .= "\n        ]\n";
+            $this->buffer .= "      }";
+            if ( $boucle < sizeof($this->item_list) -1 ){
+              $this->buffer .= ",\n";
+            }
+            else {$this->buffer .= ",";}
+            
+            
+            break;
+
+            default:
+              if ( empty($attributes['label']) ) { $attributes['label']="";}
+              
+              $this->buffer .= "      {\n";
+              $this->buffer .= "key:\"$item\",\n";
+              //, label:"'.$attributes['label'].'"';
+              foreach ($attributes as $att_key => $att_value) {
+                $this->buffer .= "$att_key:$att_value,\n";
+              }
+              $this->buffer .= "      }\n";
+              if ( $boucle < sizeof($this->item_list) ){
+                $this->buffer .= ",\n";
+              }
+
+        }
+      }
+      $boucle++;
+    }
 /*
             \"Title\",
             \"Phone\",
             { key:\"Rating.AverageRating\", label:\"Rating\" }
             
 */
-				$this->buffer .="
+    $this->buffer .="
     ],\n
-    summary: \"Pizza places near 98089\",
-    caption: \"Table with JSON data from YQL\"
+    summary: \"".$this->ajax_info['table_summary']."\",
+    caption: \"".$this->ajax_info['table_caption']."\"
     });
     ";
 
-				$add_request = "";
-				if ( isset($this->ajax_info['params']) && gettype($this->ajax_info['params']) == "array" ){
-					foreach ( $this->ajax_info['params'] as $key => $value ){
-						if ( $add_request != "" ){ $add_request .= "&";}
-						$add_request .= "$key=$value";
-					}
-				}
+    $add_request = "";
+    if ( isset($this->ajax_info['params']) && gettype($this->ajax_info['params']) == "array" ){
+      foreach ( $this->ajax_info['params'] as $key => $value ){
+        if ( $add_request != "" ){ $add_request .= "&";}
+        $add_request .= "$key=$value";
+      }
+    }
 
-				$this->buffer .= "\n";
-				$this->buffer .= "table_".$this->name.".plug(Y.Plugin.DataTableDataSource, { datasource: DS_".$this->name." });\n";
-				$this->buffer .= "table_".$this->name.".datasource.load({ request: \"startIndex=0&resultCount=10&$add_request\" })\n";
-				$this->buffer .= "DS_".$this->name.".after(\"response\", function(){ table_".$this->name.".render(\"#".$this->name."\") });";
-				$this->buffer .= "table_".$this->name.".render(\"#".$this->name."\");\n";
-				$this->buffer .= "\n";
-				
+    $this->buffer .= "\n";
+    $this->buffer .= "table_".$this->name.".plug(Y.Plugin.DataTableDataSource, { datasource: DS_".$this->name." });\n";
+    $this->buffer .= "table_".$this->name.".datasource.load({ request: \"startIndex=0&results=10&$add_request\" })\n";
+    $this->buffer .= "DS_".$this->name.".after(\"response\", function(){ table_".$this->name.".render(\"#".$this->name."\") });";
+    $this->buffer .= "table_".$this->name.".render(\"#".$this->name."\");\n";
+    $this->buffer .= "\n";
+
   }
-	
+
 }
-
-
-
-
 
