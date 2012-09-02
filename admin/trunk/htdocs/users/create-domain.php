003e71d949eacc5a6574571584855f30fa91fd29
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-09-02 19:51:28 +0000

    Add the user class to check current user.
    git-svn-id: file:///svn/openvisp@605 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/users/create-domain.php
+++ admin/trunk/htdocs/users/create-domain.php
@@ -45,11 +45,26 @@ require ("../lib/functions.inc.php");
 require ("../lib/accounts.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+
 $SESSID_USERNAME = check_user_session ();
 
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$user_info->fetch_info($SESSID_USERNAME);
+$user_info->check_domain_admin();
+
+/*
 if ( check_domain_admin($SESSID_USERNAME) == false ){
   header ("Location: main.php");
 }
+*/
+
+
 
 $account_information = get_account_info($SESSID_USERNAME);
 $account_quota = get_account_quota($account_information['id']);
