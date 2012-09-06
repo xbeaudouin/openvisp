3cf8cd57c91595108ecee4d3def9dd9566b5c073
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-09-06 19:17:24 +0000

    Optimise some code on fetchdomain info by name or id Create a destruct method Create the method add_domain_alias, domain_alias_exist, fetch_domain_alias Change some code type to make it more readable by text editor.
    git-svn-id: file:///svn/openvisp@619 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/lib/domain.class.php
+++ admin/trunk/htdocs/lib/domain.class.php
@@ -19,12 +19,22 @@ class DOMAIN
 	{
 		$query = "SELECT * FROM domain WHERE domain='$domain_name'";
 		$result = $this->db_link->sql_query($query);
-		$this->data_domain = $result['result'][0];
-		$this->domain_name = $this->data_domain['domain'];
-		$this->storhash();
-		$this->fetch_quota();
-		$this->fetch_quota_status();
+    if ( $result['rows'] == 1 ){
+      $this->data_domain = $result['result'][0];
+      $this->domain_name = $this->data_domain['domain'];
+      $this->domain_status = 1;
+      $this->storhash();
+      $this->fetch_quota();
+      $this->fetch_quota_status();
+    }
+    else{
+      $this->domain_status = 0;
+    }
 	}
+
+  function __destruct() {
+  }
+
 
 	//
 	// fetch_by_domainid
@@ -35,11 +45,18 @@ class DOMAIN
 	{
 		$query = "SELECT * FROM domain WHERE id='$domain_id'";
 		$result = $this->db_link->sql_query($query);
-		$this->data_domain = $result['result'][0];
-		$this->domain_name = $this->data_domain['domain'];
-		$this->storhash();
-		$this->fetch_quota();
-		$this->fetch_quota_status();
+    if ( $result['rows'] == 1 ){
+      $this->data_domain = $result['result'][0];
+      $this->domain_name = $this->data_domain['domain'];
+      $this->domain_status = 1;
+      $this->storhash();
+      $this->fetch_quota();
+      $this->fetch_quota_status();
+      $this->fetch_policy_id();
+    }
+    else{
+      $this->domain_status = 0;
+    }
 	}
 
 	//
@@ -351,9 +368,11 @@ class DOMAIN
 	//
   function fetch_policy(){
 
-    $query = "SELECT policy.id
-FROM policy
-WHERE id=".$this->policy_id;
+    $query = "
+    SELECT policy.id
+    FROM policy
+    WHERE id='".$this->policy_id."'
+    ";
 
     $result = $this->db_link->sql_query($query);
 		$this->policy = $result['result'];
@@ -362,10 +381,12 @@ WHERE id=".$this->policy_id;
   }
 
 	function domain_exist($domain_name){
-		$query = "SELECT domain
-FROM domain
-WHERE domain='".$domain_name."'
-";
+
+		$query = "
+    SELECT domain
+    FROM domain
+    WHERE domain='".$domain_name."'
+    ";
 
     $result = $this->db_link->sql_query($query);
 		debug_info("$query");
@@ -410,8 +431,9 @@ WHERE domain='".$domain_name."'
 
 		if ( $error == 0 ){
 
-			$query = "INSERT INTO domain (domain,description,aliases,mailboxes,maxquota,backupmx,antivirus,vrfydomain,vrfysender,greylist,spf,created,modified,active)
-VALUES ('".$domain_array['name']."','".$domain_array['description']."','".$domain_array['mailbox_aliases']."','".$domain_array['mailboxes']."','".$domain_array['maxquota']."','".$domain_array['backupmx']."','".$domain_array['antivirus']."','".$domain_array['vrfydomain']."','".$domain_array['vrfysender']."','".$domain_array['greylisting']."','".$domain_array['spf']."',NOW(),NOW(),'".$domain_array['active']."')";
+			$query = "
+      INSERT INTO domain (domain,description,aliases,mailboxes,maxquota,backupmx,antivirus,vrfydomain,vrfysender,greylist,spf,created,modified,active)
+      VALUES ('".$domain_array['name']."','".$domain_array['description']."','".$domain_array['mailbox_aliases']."','".$domain_array['mailboxes']."','".$domain_array['maxquota']."','".$domain_array['backupmx']."','".$domain_array['antivirus']."','".$domain_array['vrfydomain']."','".$domain_array['vrfysender']."','".$domain_array['greylisting']."','".$domain_array['spf']."',NOW(),NOW(),'".$domain_array['active']."')";
 
 			$result = $this->db_link->sql_query($query);
 
@@ -482,7 +504,7 @@ VALUES ('".$domain_array['name']."','".$domain_array['description']."','".$domai
 		global $PALANG;
 
 		$query = "INSERT INTO policy (domain_id, virus_lover, spam_lover, banned_files_lover, bad_header_lover, bypass_virus_checks, bypass_spam_checks, bypass_banned_checks, bypass_header_checks, spam_modifies_subj, virus_quarantine_to, spam_quarantine_to, banned_quarantine_to, bad_header_quarantine_to, spam_tag_level, spam_tag2_level, spam_kill_level, spam_dsn_cutoff_level, addr_extension_virus, addr_extension_spam, addr_extension_banned, addr_extension_bad_header, warnvirusrecip, warnbannedrecip, warnbadhrecip, newvirus_admin, virus_admin, banned_admin, bad_header_admin, spam_admin, spam_subject_tag, spam_subject_tag2, message_size_limit, banned_rulenames)
-VALUES ('".$this->data_domain['id']."','".$policy_array['virus_lover']."','".$policy_array['spam_lover']."','".$policy_array['banned_files_lover']."','".$policy_array['bad_header_lover']."','".$policy_array['antivirus']."','".$policy_array['spam_lover']."','".$policy_array['bypass_banned_checks']."','".$policy_array['bypass_header_checks']."','Y','".$CONF['virus_quarantine_to']."','".$CONF['spam_quarantine_to']."','".$CONF['banned_quarantine_to']."','','".$CONF['sa_tag_level']."','".$CONF['sa_tag2_level'] ."','".$CONF['sa_kill_level'] ."','','','','','','N','N','N','','','','','','".$CONF['spam_subject_tag']."','".$CONF['spam_subject_tag2']."','','')";
+    VALUES ('".$this->data_domain['id']."','".$policy_array['virus_lover']."','".$policy_array['spam_lover']."','".$policy_array['banned_files_lover']."','".$policy_array['bad_header_lover']."','".$policy_array['antivirus']."','".$policy_array['spam_lover']."','".$policy_array['bypass_banned_checks']."','".$policy_array['bypass_header_checks']."','Y','".$CONF['virus_quarantine_to']."','".$CONF['spam_quarantine_to']."','".$CONF['banned_quarantine_to']."','','".$CONF['sa_tag_level']."','".$CONF['sa_tag2_level'] ."','".$CONF['sa_kill_level'] ."','','','','','','N','N','N','','','','','','".$CONF['spam_subject_tag']."','".$CONF['spam_subject_tag2']."','','')";
 
 		$result = $this->db_link->sql_query($query);
 
@@ -616,9 +638,10 @@ VALUES ('".$this->data_domain['id']."','".$policy_array['virus_lover']."','".$po
 		global $PALANG;
 		global $user_info;
 
-		$query = "INSERT INTO domain_admins (accounts_id, domain_id, active)
-VALUES (".$user_info->data['id'].", ".$this->data_domain['id']." , 1 )
-";
+		$query = "
+    INSERT INTO domain_admins (accounts_id, domain_id, active)
+    VALUES (".$user_info->data['id'].", ".$this->data_domain['id']." , 1 )
+    ";
 
     $result = $this->db_link->sql_query($query);
 
@@ -705,6 +728,132 @@ VALUES (".$user_info->data['id'].", ".$this->data_domain['id']." , 1 )
 		return $directory;
 
 	}
+
+
+  //
+  //  fetch_domain_alias
+  //  Action: fetch informations about domain alias
+  //  Call: fetch_domain_alias (domain alias name)
+  function fetch_domain_alias ($domain_alias_name) {
+
+    $query = "
+    SELECT *
+    FROM domain_alias
+    WHERE domain_id='".$this->data_domain['id']."'
+      AND dalias='".$domain_alias_name."'
+    ";
+
+    $result = $this->db_link->sql_query($query);
+    $this->domain_alias_status = 0;
+
+    if ( $result['rows'] == 1 ){
+      $this->domain_alias = $result['result'][0];
+      $this->domain_alias_status = 1;
+    }
+
+
+  }
+
+
+  //
+  //  domain_alias_exist
+  //  Action: check if a domain alias exist
+  //  Call: domain_alias_exist (domain alias name)
+  function domain_alias_exist ($domain_alias_name) {
+
+    $query = "
+    SELECT dalias
+    FROM domain_alias
+    WHERE domain_id='".$this->data_domain['id']."'
+      AND dalias='".$domain_alias_name."'
+    ";
+
+    $result = $this->db_link->sql_query($query);
+    
+    if ( $result['rows'] == 1 ){
+      return TRUE;
+    }
+    else{
+      return FALSE;
+    }
+
+
+  }
+
+
+  //
+  //  add_domain_alias
+  //  Action: add a new domain_alias
+  //  Call: add_domain_alias (domain alias name)
+  function add_domain_alias ($domain_alias_name) {
+
+    global $PALANG;
+
+    $domain_alias_name = strtolower($domain_alias_name);
+
+    if ( empty($domain_alias_name) ){
+      $this->msg['status'] = 0;
+      $this->msg['text'] = $PALANG['pCreate_domain_alias_text_error1'];
+      return false;
+    }
+
+    if ( ($this->domain_exist($domain_alias_name)) ){
+      $this->msg['status'] = 0;
+      $this->msg['text'] = $PALANG['pCreate_domain_alias_text_error2']."($domain_alias_name)";
+      return FALSE;
+    }
+
+    if ( ($this->domain_alias_exist($domain_alias_name)) ){
+      $this->msg['status'] = 0;
+      $this->msg['text'] = $PALANG['pCreate_domain_alias_text_error3'];
+      return false;
+    }
+
+
+    $query = "
+    INSERT INTO domain_alias (dalias,domain_id,active,created)
+    VALUES ('".$domain_alias_name."',".$this->data_domain['id'].",1,NOW())
+    ";
+
+    $result = $this->db_link->sql_query($query);
+    if ( $result['rows'] == 1 ){
+      $this->msg['status'] = 1;
+      $this->msg['text'] = $PALANG['pCreate_domain_alias_result_success'];
+    }
+    else{
+      $this->msg['status'] = 0;
+      $this->msg['text'] = $PALANG['DB_Error']."//".$this->msg['status']; 
+      return;
+    }
+
+    $query = "
+    INSERT INTO alias (address,goto,policy_id,created,active)
+    VALUES ('@".$domain_alias_name."','@".$this->data_domain['domain']."','".$this->data['policy_id']."',NOW(),'1')
+    ";
+
+    $result = $this->db_link->sql_query($query,2);
+
+    if ( $result['rows'] == 1 ){
+      $this->msg['status'] = 1;
+      $this->msg['text'] = $PALANG['pCreate_domain_alias_result_success'];
+      $this->db_link->db_log("create domain alias", "$domain_alias_name -> ".$this->data_domain['domain'],"'.$this->data_domain['domain'].'");
+    }
+    else{
+
+      $query = "
+      DELETE FROM domain_alias
+      WHERE dalias = '".$domain_alias_name."'
+        AND domain_id = ".$this->data_domain['id']."
+      ";
+      $result = $this->db_link->sql_query($query);
+
+      $this->msg['status'] = 0;
+      $this->msg['text'] = $PALANG['DB_Error']; 
+    }
+
+    return;
+
+  }
 
 
 }
