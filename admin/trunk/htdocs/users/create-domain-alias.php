4772dd44daf09995b0d04358db3ac71eecf551af
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-09-06 19:15:35 +0000

    Fix typo Fix indent Remove useless template Optimize code by using object to avoid duplicate code.
    git-svn-id: file:///svn/openvisp@618 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/users/create-domain-alias.php
+++ admin/trunk/htdocs/users/create-domain-alias.php
@@ -38,91 +38,41 @@ $list_domains = list_domains_local ();
 
 
 $ovadb = new DB();
-$userinfo = new USER($ovadb);
-$userinfo->fetch_info ($SESSID_USERNAME);
-$userinfo->check_access("domain");
+$user_info = new USER($ovadb);
+$user_info->fetch_info ($SESSID_USERNAME);
+$user_info->check_access("domain");
 
 $domain_info = new DOMAIN($ovadb);
-$userinfo->check_quota("domains");
+$user_info->fetch_quota_status();
+
 $body_class = 'class="yui3-skin-sam"';
+
 
 if ($_SERVER['REQUEST_METHOD'] == "GET")
 {
-   $pCreate_domain_alias_goto_text = $PALANG['pCreate_domain_alias_goto_text'];
+  $pCreate_domain_alias_goto_text = $PALANG['pCreate_domain_alias_goto_text'];
 
-	 $tDomain = get_get('domain');
-  
-   include ("../templates/header.tpl");
-   include ("../templates/users/menu.tpl");
-   include ("../templates/users/create-domain-alias.tpl"); 
-   include ("../templates/footer.tpl");
+  $tDomain = get_get('domain');
+
+  include ("../templates/header.tpl");
+  include ("../templates/users/create-domain-alias.tpl"); 
+  include ("../templates/footer.tpl");
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "POST")
-{
-   $pCreate_domain_alias_goto_text = $PALANG['pCreate_domain_alias_goto_text'];
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
-   $fNewDomain = strtolower (get_post('fNewDomain'));
-   $fDomain_id = get_post('fDomain_id');
-	 //	 $domain_id = get_domain_id($fDomain);
-	 //$domain_policy = get_domain_policy($fDomain);
+  $pCreate_domain_alias_goto_text = $PALANG['pCreate_domain_alias_goto_text'];
+
+  $domain_info->fetch_by_domainid(get_post('fDomain_id'));
+  $domain_info->add_domain_alias(get_post('fNewDomain'));
 
 
-	 $domain = new DOMAIN($ovadb);
-	 $domain->fetch_by_domainid($fDomain_id);
+  $tMessage = $domain->msg['text'];
 
+  include ("../templates/header.tpl");
+  include ("../templates/users/create-domain-alias.tpl");
+  include ("../templates/footer.tpl");
 
-   if (empty($fNewDomain))
-   {
-     $error = 1;
-     $tNewDomain = get_post('fNewDomain');
-     $tDomain = $fDomain;
-     $tMessage = $PALANG['pCreate_domain_alias_text_error1'];
-   }
-   if (domain_exist($fNewDomain))
-   {
-     $error = 1;
-     $tNewDomain = get_post('fNewDomain');
-     $tDomain = $fDomain;
-     $tMessage = $PALANG['pCreate_domain_alias_text_error2'];
-   }
-
-
-
-   $result = db_query ("SELECT * FROM domain_alias WHERE dalias='$fNewDomain'");
-   if ($result['rows'] == 1)
-   {
-      $error = 1;
-      $tMessage = $PALANG['pCreate_domain_alias_text_error3'];
-   }
-
-   if ($error != 1)
-   {
-      $result = db_query ("INSERT INTO domain_alias (dalias,domain_id,created) VALUES ('$fNewDomain','$fDomain_id',NOW())");
-      if ($result['rows'] != 1)
-      {
-         $tDomain = $fDomain;
-         $tMessage = $PALANG['pCreate_domain_alias_result_error'] . "<br />($fAddress -> $fGoto)<br />";
-      }
-      else
-      {
-         db_log ($SESSID_USERNAME, $domain->data_domain['domain'], "create domain alias", "$fNewDomain -> ".$domain->data_domain['domain']);
-
-         $tDomain = $fDomain;
-				 $result = db_query ("INSERT INTO alias (address,goto,policy_id,created,active) VALUES ('@$fNewDomain','@".$domain->data_domain['domain']."','".$domain->data_domain['policy_id']."',NOW(),'1')");
-				 if ($result['rows'] != 1)
-					 {
-						 $tMessage = $PALANG['pCreate_domain_alias_result_error'] . "<br />($fAddress -> $fGoto)<br />";
-					 }
-				 else{
-					 $tMessage = $PALANG['pCreate_domain_alias_result_succes'] . "<br />($fNewDomain -> ".$domain->data_domain['domain'].")</br />";
-				 } 
-      }
-   }
-
-   include ("../templates/header.tpl");
-   include ("../templates/users/menu.tpl");
-   include ("../templates/users/create-domain-alias.tpl");
-   include ("../templates/footer.tpl");
 }
+
 ?>
