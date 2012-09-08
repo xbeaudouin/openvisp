977e53f965d43379bfe7ac467e648c2908ef1ac7
ngoralski <ngoralski@bed1d25c-e804-df11-9dd7-000c2933e665>
2012-09-08 09:49:50 +0000

    Remove old yui stuff
    git-svn-id: file:///svn/openvisp@624 bed1d25c-e804-df11-9dd7-000c2933e665

--- admin/trunk/htdocs/lib/yui3.5.0pr2/uploader/uploader-min.js
+++
@@ -1,7 +0,0 @@
-/*
-YUI 3.5.0pr2 (build 4560)
-Copyright 2011 Yahoo! Inc. All rights reserved.
-Licensed under the BSD License.
-http://yuilibrary.com/license/
-*/
-YUI.add("uploader",function(b){var a=b.config.win;if(a&&a.File&&a.FormData&&a.XMLHttpRequest){b.Uploader=b.UploaderHTML5;}else{b.Uploader=b.UploaderFlash;}},"3.5.0pr2",{requires:["uploader-flash","uploader-html5"]});
