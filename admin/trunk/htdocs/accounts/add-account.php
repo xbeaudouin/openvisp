8487c9938d867259752c059ea9a2c370cd511ab8
Nicolas GORALSKI <nicolas@goralski.fr>
2012-12-31 22:12:44 +0100

    Add the required lib to use the new menu

--- admin/trunk/htdocs/accounts/add-account.php
+++ admin/trunk/htdocs/accounts/add-account.php
@@ -18,12 +18,27 @@ require ("../lib/functions.inc.php");
 require ("../lib/accounts.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
+
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+//require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+require_once ("../lib/admin.class.php");
+
+$SESSID_USERNAME = check_user_session ();
 $SESSID_USERNAME = check_admin_session ();
+
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$user_info->fetch_info($SESSID_USERNAME);
+$user_info->fetch_quota_status();
+
+$body_class = 'class="yui3-skin-sam"';
 
 if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
    include ("../templates/header.tpl");
-   include ("../templates/accounts/menu.tpl");
    include ("../templates/accounts/add-account.tpl");
    include ("../templates/footer.tpl");
 }
@@ -129,7 +144,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		 }
    
    include ("../templates/header.tpl");
-   include ("../templates/accounts/menu.tpl");
    include ("../templates/accounts/add-account.tpl");
    include ("../templates/footer.tpl");
  }
