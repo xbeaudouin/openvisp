55b563db56e6eac3da0318bfc99fecdf1810a33d
Nicolas GORALSKI <nicolas@goralski.fr>
2013-02-12 22:53:02 +0100

    Add required code to display the page with new template

--- admin/trunk/htdocs/server/manage.php
+++ admin/trunk/htdocs/server/manage.php
@@ -12,12 +12,35 @@
 //
 // -none-
 //
+
+require ("../variables.inc.php");
 require ("../config.inc.php");
 require ("../lib/functions.inc.php");
-//require ("../lib/hosting.inc.php");
+require ("../lib/accounts.inc.php");
 include ("../languages/" . check_language () . ".lang");
 
-$SESSID_USERNAME = check_user_session ();
+require_once ("MDB2.php");
+require_once ("../lib/db.class.php");
+require_once ("../lib/user.class.php");
+require_once ("../lib/domain.class.php");
+require_once ("../lib/ajax_yui.class.php");
+require_once ("../lib/ova.class.php");
+
+
+$ovadb = new DB();
+$user_info = new USER($ovadb);
+$ova = new OVA($ovadb); 
+
+$SESSID_USERNAME = $ova->check_session();
+
+$user_info->fetch_info($SESSID_USERNAME);
+$user_info->check_domain_admin();
+$user_info->fetch_quota_status();
+
+$domain_info = new DOMAIN($ovadb);
+
+$body_class = 'class="yui3-skin-sam"';
+
 
 if ($_SERVER["REQUEST_METHOD"] == "GET")
 {
